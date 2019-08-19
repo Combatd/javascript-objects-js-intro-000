23d7699b3319775172a84a55aca5f5fb24a2830f
Mark Calvelo <combatdstar@gmail.com>
2019-08-19 02:37:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({artistName: 'song name'})
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist['artistName'] = songTitle; // Object[key] = value
 }
